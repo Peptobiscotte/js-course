8dfaf605a3ac7e66e3bda334b58edf91375e6c33
Pepto <monsieurfranci@gmail.com>
2023-08-02 17:30:45 +0200

    partial sync

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,197 @@
+'use strict'
+// COURSE 32 activating strict mode
+
+// const private = 1; // le strict mode nous previent que private est reserved donc il faut pas l'utiliser comme nom de variable
+
+// // COURSE 33 Functions
+
+// function logger() {
+//     console.log("My name Jeff")
+// }
+// // pour appeler la fonction on ecrit :
+// logger();
+
+// function fruitProcessor(pommes, oranges) {
+//     console.log(pommes, oranges)
+//     const juice = `Jus de ${pommes} pommes et ${oranges} oranges. 🧃`;
+//     return juice;
+// }
+
+// const jusPomme = fruitProcessor(5, 0);
+// console.log(jusPomme)
+// const jusOrange = fruitProcessor(0, 5);
+// console.log(jusOrange)
+// const jusMix = fruitProcessor(2, 2);
+// console.log(jusMix)
+
+
+// function describeCountry(country, population, capitalCity) {
+//     const countryData =
+//         `${country} has ${population} people and its capital is ${capitalCity}`
+//     return countryData;
+// }
+
+// const countryFr = describeCountry("France", 70000000, "Paris")
+// console.log(countryFr)
+// const countryDe = describeCountry("Germany", 80000000, "Berlin")
+// console.log(countryDe)
+
+// COURSE 34 Functions Declarations vs Functions expressions
+
+// function calculAge(birthyear) {
+//     // const age = 2023 - 1995;
+//     // return age; // pas besoin de store la const age on peut simplement ecrire:
+//     return 2023 - birthyear;
+// } // ceci est une fonction declaration une des particularites est qu'lle peut etre appelee avant d'etre declaree
+
+// const age1 = calculAge(1995);
+// console.log(age1);
+
+// // en dessous une fonction expression on ne donne pas de nom a la fonction mais on la store dans une const - on est obliges de la declarer avant de l'appeler ce qui peut rendre le code plus propre si on declare toutes les fonctions au top du code avant le reste.
+// const calcAge2 = function (birthyear) {
+//     return 2023 - birthyear;
+// }
+
+// const age2 = calcAge2(1995);
+// console.log(age2)
+
+
+// // assignement declaration :
+// function percentageOfWorld1(population) {
+//     return (population / 7900000000) * 100;
+// }
+// const china = percentageOfWorld1(1412000000)
+// const india = percentageOfWorld1(1408000000)
+// const france = percentageOfWorld1(67000000)
+
+// console.log(china, india, france)
+
+
+// //assignement expression :
+// const percentageOfWorld2 = function (population) {
+//     return (population / 79000000000) * 100;
+// }
+// const china2 = percentageOfWorld2(14120000000)
+// const india2 = percentageOfWorld2(14080000000)
+// const france2 = percentageOfWorld2(67000000)
+
+// console.log(china2, india2, france2)
+
+// COURSE 35 arrow functions
+
+// //arrow function :
+// const calcAge3 = birthYear => 2023 - birthYear; // fomre la plus concise on a pas besoin de {} ni de return, le return est implicite
+// //utile pour les functions courtes avec une seule variable
+// const age3 = calcAge3(1995)
+// console.log(age3)
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2023 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement} years. 😭`; // ici on utilise {} et return car on a plusieurs lignes de code.
+// }
+// const maxRetirement = yearsUntilRetirement(1995, "Paxit")
+// const dadRetirement = yearsUntilRetirement(1967, "Eric")
+// console.log(maxRetirement, dadRetirement)
+
+// //asignement
+
+// const percentageOfWorld3 = population => (population / 7900000000) * 100
+// console.log(percentageOfWorld3(1412000000)) // on aurait aussi pu store la variable china dans laquelle on rentre la function avec la valeur population associee a la chine :
+// const chinaPercentagePop = percentageOfWorld3(1412000000)
+// console.log(chinaPercentagePop);
+
+// COURSE 36 Functions calling others
+// function cutFruitPieces(fruit) {
+//     return fruit * 4
+// }
+
+// const fruitProcessor = function (pommes, oranges) {
+
+//     const pommesPieces = cutFruitPieces(pommes);
+//     const orangesPieces = cutFruitPieces(oranges);// ici on appelle la fonction du dessus dans la seconde fonction.
+
+//     const juice = `Jus de ${pommesPieces} morceaux de pommes et ${orangesPieces} morceaux d'oranges. 🧃`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2, 3))
+
+
+// // assignement
+
+// const percentageTotalPop = function (populationSpe) {
+//     return (populationSpe / 7900000000) * 100
+// }
+// const describePopulation = function (country, population) {
+//     const percentagePop = percentageTotalPop(population)
+//     const resultPop = `${country} has ${population} people which is about ${percentagePop} of the world`
+//     return resultPop;
+// }
+// console.log(describePopulation("China", 1441000000))
+
+// COURSE 37 reviewing functions
+
+// const calcAgeMan = function (birthYear) {
+//     return 2023 - birthYear;
+// }
+// const AlexIsPd = function (firstName) {
+//     if (firstName === "Alex") {
+//         const resultAlex = `${firstName}("nom de pd")`
+//         return resultAlex;
+//     } else {
+//         return firstName;
+//     }
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAgeMan(birthYear);
+//     const alexVar = AlexIsPd(firstName);
+//     const retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         console.log(retirement)
+//         return `${firstName, alexVar} retires in ${retirement} years. 😭`;
+//     } else {
+//         console.log(retirement)
+//         return `${firstName} is already retired`
+//     }
+
+
+// }
+// console.log(yearsUntilRetirement(1995, "Alex"))
+
+// COURSE 38 Coding challenge # 1
+
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// let scoreDolphins = calcAverage(44, 23, 71)
+// let scoreKoalas = calcAverage(65, 54, 49)
+// console.log(scoreDolphins, scoreKoalas)
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//     if (avgDolphins >= 2 * avgKoalas) {
+//         console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`)
+//     } else if (avgKoalas >= 2 * avgDolphins) {
+//         console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
+//     } else {
+//         console.log("no winner")
+//     }
+// }
+// checkWinner(scoreDolphins, scoreKoalas)
+// checkWinner(100, 200)
+
+// scoreDolphins = calcAverage(85, 54, 41)
+// scoreKoalas = calcAverage(23, 34, 27)
+// console.log(scoreDolphins, scoreKoalas)
+
+// checkWinner(scoreDolphins, scoreKoalas)
+
+// COURSE 39 intro to arrays
+
+const friend1 = "Jeff"
+const friend2 = "Peter"
+const friend3 = "Steven"
+// chiant de les rentrer un par un donc on utilise un type de data storage qui s'appelle Array:
+
+const friends = ["Jeff", "Peter", "Steven"];
+console.log(friends)
