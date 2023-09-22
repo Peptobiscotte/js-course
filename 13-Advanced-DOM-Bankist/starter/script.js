a09adefd34ac0fc17923e5574049d92d2e4b45f2
Pepto <monsieurfranci@gmail.com>
2023-09-22 18:19:36 +0200

    finished s13

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -142,6 +142,128 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 })
 headerObserver.observe(header)
 
+// -reveal sections-
+const allSections = document.querySelectorAll('.section')
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries
+  if (!entry.isIntersecting) return
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null, // viewport
+  threshold: 0.15
+})
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section)
+  section.classList.add('section--hidden') // ⚠️ réétablir
+})
+
+// -lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]') // on selectionne uniquement les images
+// ayant le data attributes src
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries
+  if (!entry.isIntersecting) return
+  // replace src with data src
+  entry.target.src = entry.target.dataset.src
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img')
+  })
+  observer.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+})
+
+imgTargets.forEach(img => imgObserver.observe(img))
+
+// -Slider-
+const slider = function () {
+  const slides = document.querySelectorAll('.slide')
+  const btnLeft = document.querySelector('.slider__btn--left')
+  const btnRight = document.querySelector('.slider__btn--right')
+  const dotContainer = document.querySelector('.dots')
+
+  let curSlide = 0
+  const maxSlide = slides.length
+
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+    // eslint-disable-next-line max-len
+      dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`)
+    })
+  }
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'))
+
+    document.querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active')
+  }
+
+  const goToSlide = function (slide) {
+    slides.forEach((s, i) => {
+      s.style.transform =
+  `translateX(${100 * (i - slide)}%)`
+    })
+  }
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0
+    } else {
+      curSlide++
+    }
+    goToSlide(curSlide)
+    activateDot(curSlide)
+  }
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1
+    } else {
+      curSlide--
+    }
+    goToSlide(curSlide)
+    activateDot(curSlide)
+  }
+
+  const init = function () {
+    createDots()
+    goToSlide(0)
+    activateDot(0)
+  }
+  init()
+
+  // Event handlers
+  btnRight.addEventListener('click', nextSlide)
+  btnLeft.addEventListener('click', prevSlide)
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide()
+    if (e.key === 'ArrowRight') nextSlide()
+  })
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset
+      goToSlide(slide)
+      activateDot(slide)
+    }
+  })
+}
+slider()
+
 /// /////////////////////////////////////////////////////////////////////////
 /// /////////////////////////////////////////////////////////////////////////
 /// /////////////////////////////////////////////////////////////////////////
@@ -374,3 +496,26 @@ btnScrollTo.addEventListener('click', function (e) {
 
 // const observer = new IntersectionObserver(obsCallBack, obsOptions)
 // observer.observe(section1)
+
+// COURSE 198 revealing elem on scroll.
+
+// COURSE 199 Lazy loading images
+
+// COURSE 200 Slider component
+
+// COURSE 202 Lifecycle DOM events
+
+// document.addEventListener('DOMContentLoaded', function (e) {
+//   console.log('HTML parsed DOM tree built', e)
+// })
+
+// window.addEventListener('load', function (e) {
+//   console.log('Page loaded', e) // event au loading
+// })
+
+// window.addEventListener('beforeunload', function (e) { // event juste avant de quitter la page.
+//   e.preventDefault() // exemple petite croix
+//   e.returnValue = ''
+// })
+
+// COURSE 203 Efficient script loading
