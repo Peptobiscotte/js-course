6c581ad1ff2af12910609a4be7a9934f4d50926a
Pepto <monsieurfranci@gmail.com>
2023-10-05 15:58:22 +0200

    end of s15

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -70,11 +70,15 @@ class Cycling extends Workout {
 // ARCHITECTURE
 class App {
   #map
-  #mapZoomLevel = 13
+  #mapZoomLevel = 14
   #mapEvent
   #workouts = []
   constructor () {
+    // get user position
     this._getPosition()
+    // Get date from local storage
+    this._getLocalStorage()
+    // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this))
     inputType.addEventListener('change', this._toggleElevationField)
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this))
@@ -104,6 +108,10 @@ class App {
     }).addTo(this.#map)
 
     this.#map.on('click', this._showForm.bind(this))
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work)
+    })
   }
 
   _showForm (mapE) {
@@ -171,7 +179,8 @@ class App {
     // Clear input fields
     this._hideForm()
 
-    // Display Marker
+    // Set local storage to all workouts
+    this._setLocalStorage()
   }
 
   _renderWorkoutMarker (workout) {
@@ -248,8 +257,27 @@ class App {
         duration: 1
       }
     })
+  }
 
-    workout.click()
+  _setLocalStorage () {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts))
+  }
+
+  _getLocalStorage () {
+    const data = JSON.parse(localStorage.getItem('workouts'))
+
+    if (!data) { return }
+
+    this.#workouts = data
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work)
+    })
+  }
+
+  reset () {
+    localStorage.removeItem('workouts')
+    location.reload()
   }
 }
 
